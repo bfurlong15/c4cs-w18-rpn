4fac264283bbe936d969c26e058ae282791918cc
bfurlong <bfurlong@umich.edu>
2018-03-04 12:14:51 -0500

    init commit

--- rpn.py
+++ rpn.py
@@ -1,20 +1,83 @@
 #!/usr/bin/env python3
 
-def calculate(arg):
-    stack = list();
-    for token in arg.split():
-    	try:
-    		value = int(token)
-    		stack.append(value)
-    	except ValueError:
-    		arg1 = stack.pop()
-    		arg2 = stack.pop()
-    		return arg1 + arg2
-    	
+#from colorama import init
+#init()
+#from colorama import Fore, Back, Style
+
+import readline
+import operator
+import math
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            #function = operators[token]
+            #arg2 = stack.pop()
+            #arg1 = stack.pop()
+            #result = function(arg1, arg2)
+            #stack.append(result)
+            
+            if token == '+':
+            	arg2 = stack.pop()
+            	arg1 = stack.pop()
+            	return arg1 + arg2
+            if token == '-':
+            	arg1 = stack.pop()
+            	arg2 = stack.pop()
+            	return arg2 - arg1
+            if token == '*':
+            	arg1 = stack.pop()
+            	arg2 = stack.pop()
+            	return arg1 * arg2
+            if token == '/':
+            	arg1 = stack.pop()
+            	arg2 = stack.pop()
+            	return arg2 / arg1
+            if token == '!':
+            	arg1 = stack.pop()
+            	return math.factorial(arg1)
+            if token == 'sin':
+            	arg1 = stack.pop()
+            	return math.sin(arg1)
+            if token == 'cos':
+            	arg = stack.pop()
+            	return math.cos(arg)
+            if token == 'tan':
+            	arg = stack.pop()
+            	return math.tan(arg)
+            if token == 'copy':
+            	arg = stack.pop();
+            	stack.append(arg)
+            	stack.append(arg)
+            	print(stack)
+            	size = len(stack)
+            	
+            	return size
+            	
+            
+        print(stack)
+    #if len(stack) != 1:
+     #   raise TypeError("Too many parameters")
+    #return stack.pop()
 
 def main():
     while True:
-        print(calculate(input("rpn calc> ")))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
+
+    
+#deinit()
--- test_rpn.py
+++ test_rpn.py
@@ -4,3 +4,18 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+    def test_factorial(self):
+    	result = rpn.calculate("4 !")
+    	self.assertEqual(24, result)
+    def test_sin(self):
+    	result = rpn.calculate("0 sin")
+    	self.assertEqual(0, result)
+    def test_cos(self):
+    	result = rpn.calculate("0 cos")
+    	self.assertEqual(1, result)
+    def test_tan(self):
+    	result = rpn.calculate("0 tan")
+    	self.assertEqual(0, result)
+    def test_copy(self):
+    	result = rpn.calculate("5 6 3 copy")
+    	self.assertEqual(4, result)
